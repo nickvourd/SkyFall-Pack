4f1ac8dcdb807ff66a87f8da8c95629ed10c122c
Nikos Vourdas <32869078+nickvourd@users.noreply.github.com>
2025-03-01 13:58:51 -0600

    Update compute.tf

--- Terraform-Pack/compute.tf
+++ Terraform-Pack/compute.tf
@@ -20,8 +20,8 @@ resource "azurerm_linux_virtual_machine" "main" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
+    offer     = "0001-com-ubuntu-server-noble"
+    sku       = "24_04-lts-gen2"
     version   = "latest"
   }
 }
